c06a530f19aae3b9f4c4c71fccbe20a57522abdf
Colby Norwood <colbynorwood@Colbys-MacBook-Pro.local>
2024-03-19 23:33:57 -0500

    read and create work, update and delete do not

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const { ObjectId } = require('mongodb')
 const port = (process.env.PORT || 5500)
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
 
 
 // set the view engine to ejs
@@ -15,6 +17,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure: true}
+}))
 
 // use res.render to load up an ejs view file
 
@@ -28,13 +36,30 @@ const client = new MongoClient(process.env.URI, {
     }
 });
 
-async function connectClarksGym() {
+async function connectMemberships() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const result = await client.db("ClarksGym").collection("memberships").find().toArray();
+        console.log("mongo call await inside f/n: ", result);
+        return result;
+    }
+    catch (err) {
+        console.log("getClarksGym() error: ", err);
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+
+async function connectSchedule() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        const result = await client.db("ClarksGym").collection("memberships").find().toArray();
+        const result = await client.db("ClarksGym").collection("schedule").find().toArray();
         console.log("mongo call await inside f/n: ", result);
         return result;
     }
@@ -50,109 +75,150 @@ async function connectClarksGym() {
 // Read from Database
 app.get('/', async (req, res) => {
 
-    let result = await connectClarksGym();
+    let result = await connectMemberships();
 
+    console.log("result: ", result);
 
     res.render('index', {
         pageTitle: "Clark's Gym",
         title: "Welcome to Clark's Gym",
+        members: result
 
     });
 
 });
 
-app.get('/login', async (req, res) => {
+app.get('/login', function (req, res) {
+
     res.render('login', {
         pageTitle: "Login",
-        title: "Login to your account",
+        title: "Login to your account"
     })
 });
+app.post('/loginCheck', async (req, res) => {
+    try {
+        // check if the user exists
+        client.connect;
+        const collection = await client.db("ClarksGym").collection("memberships"); 
+        let user = await collection.findOne({ userName: req.body.userName });
+        if (user) {
+            // check if the password is correct
+            if (req.body.password === user.password) {
+                // store the user in the session
+                req.session.user = user;
+                res.redirect('/account');
+            } else {
+                res.send('Not Allowed');
+                console.log('Not Allowed');
+            }
+        } else {
+            res.send('Not Allowed');
+            console.log('Not Allowed');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-app.get('/account', async (req, res) => {
-    res.render('account', {
-        pageTitle: "Account",
-        title: "Your Account",
-    })
-} );
+app.post('/members', async (req, res) => {
+    let result = await connectMemberships();
 
-app.get('/members', async (req, res) => {
-    let result = await connectClarksGym();
+    console.log("result: ", result);
+
     res.render('members', {
         pageTitle: "Members",
-        title: "Memberships"
-    })
-});
-
-app.get('/login', async (req, res) => {
-    let result = await connectClarksGym();
-    res.render('login', {
-        pageTitle: "Login",
-        title: "Login to your account",
+        title: "Memberships",
+        members: result
     })
 });
 
 app.get('/signup', async (req, res) => {
+    
     res.render('signup', {
         pageTitle: "Sign Up",
         title: "Sign Up for a Membership",
     })
 });
 
+app.post('/signupNew', async (req, res) => {
+
+    try {
+                // console.log("req.body: ", req.body) 
+                client.connect;
+                const collection = client.db("ClarksGym").collection("memberships");
+        
+                //draws from body parser 
+                console.log(req.body);
+        
+                await collection.insertOne(req.body);
+        
+        
+                res.redirect('/signup');
+            }
+            catch (err) {
+                console.log("error")
+            }
+            finally {
+                // client.close()
+            }
+} );
+
 app.get('/reviews', async (req, res) => {
+    let result = await connectMemberships();
+
+    console.log("result: ", result);
+
     res.render('reviews', {
         pageTitle: "Reviews",
         title: "View the Reviews",
+        members: result
     })
 });
 
 app.get('/schedule', async (req, res) => {
+    let resultSchedule = await connectSchedule();
+
+    console.log("resultSchedule: ", resultSchedule);
+
     res.render('schedule', {
         pageTitle: "Schedule",
-        title: "View the Schedule",
+        title: "View the Weekly Schedule",
+        schedule: resultSchedule
     })
 });
 
+app.get('/account', async (req, res) => {
+    let result = await connectMemberships();
 
+    console.log("result: ", result);
 
-// app.get('/members', async (req, res) => {
-    
-//         let result = await connectClarksGym();
-    
-//         res.render('members', {
-//             pageTitle: "Members",
-//             members: result
-//         });
-    
-//     });
+    res.render('account', {
+        pageTitle: "Account",
+        title: "Your Account",
+        members: result
+    })
+} );
 
+app.post('/updateMember', async (req, res) => {
+    try{
+        console.log("body: ", req.body);
+        client.connect;
+        const collection = client.db("ClarksGym").collection("memberships");
+        let result = await collection.findOneAndUpdate(
+            {"_id": new ObjectId(req.body.id)},
+            {$set: {name: req.body.name, userName: req.body.userName, 
+                password: req.body.password, email: req.body.email, address: req.body.address, membership: req.body.membership}}
+        )
+        .then(result => {
+            console.log(result);
+            res.redirect('/account');
+        })
+        .catch(error => console.error(error))
+    } catch {
+        console.log("error");
+    }
 
-
-// //Create to Database
-// app.post('/addMember', async (req, res) => {
-
-//     try {
-//         // console.log("req.body: ", req.body) 
-//         client.connect;
-//         const collection = client.db("ClarksGym").collection("memberships");
-
-//         //draws from body parser 
-//         console.log(req.body);
-
-//         await collection.insertOne(req.body);
-
-
-//         res.redirect('/');
-//     }
-//     catch (err) {
-//         console.log(err)
-//     }
-//     finally {
-//         // client.close()
-//     }
-
-// });
-
-
+    });
 
 // // Update to Database
 // app.post('/updateMember', async (req, res) => {
@@ -180,30 +246,30 @@ app.get('/schedule', async (req, res) => {
 
 // });
 
-// // Delete from Database
-// app.post('/deleteMember', async (req, res) => {
+//  Delete from Database
+app.post('/deleteMember', async (req, res) => {
 
-//     try {
-//         console.log("body: ", req.body);
+    try {
+        console.log("body: ", req.body);
 
-//         client.connect;
-//         const collection = client.db("ClarksGym").collection("memberships");
-//         let result = await collection.findOneAndDelete(
-//             {
-//                 "_id": new ObjectId(req.body.id)
-//             }
-//         )
-//             .then(result => {
-//                 console.log(result);
-//                 res.redirect('/');
-//             })
-//             .catch(error => console.error(error))
-//     }
-//     finally {
-//         //client.close()
-//     }
+        client.connect;
+        const collection = client.db("ClarksGym").collection("memberships");
+        let result = await collection.findOneAndDelete(
+            {
+                "_id": new ObjectId(req.body.id)
+            }
+        )
+            .then(result => {
+                console.log(result);
+                res.redirect('/account');
+            })
+            .catch(error => console.error(error))
+    }
+    finally {
+        //client.close()
+    }
 
-// })
+})
 
 app.listen(port, () => {
     console.log(`quebec app listening on port ${port}`)
